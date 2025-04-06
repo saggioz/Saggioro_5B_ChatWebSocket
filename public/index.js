f53e66595d3529a2f0c24e2a1899b27832fe1d9f
FEDERICO <federico.saggiro@icloud.com>
2025-04-06 20:50:29 +0200

    modifiche quasi finito

--- public/index.js
+++ public/index.js
@@ -1,37 +1,62 @@
 const input = document.getElementById("input");
 const button = document.getElementById("sendButton");
 const chat = document.getElementById("chat");
+const userListElement = document.getElementById("userList");
+const setNameButton = document.getElementById("ButtonSent");
+const usernameInput = document.getElementById("username");
+const modal = document.getElementById("nameModal");
 
 const template = "<li class=\"list-group-item\">%MESSAGE</li>";
 const messages = [];
+const users = [];
 
 const socket = io();
 
-input.onkeydown = (event) => {
-  
-  if (event.keyCode === 13) {
-      event.preventDefault();
-      button.click();
+window.onload = () => {
+    modal.style.display = "block";
+};
+
+setNameButton.onclick = () => {
+  const username = usernameInput.value.trim();
+  if (username) {
+    socket.emit("setName", username);
+    modal.style.display = "none";
   }
-}
+};
 
 button.onclick = () => {
   socket.emit("message", input.value);
   input.value = "";
-}
+};
 
 socket.on("chat", (message) => {
   console.log(message);
   messages.push(message);
-  render();
-})
+  renderChat();
+});
 
-const render = () => {
+socket.on("list", (list) => {
+  users.length = 0;
+  list.forEach(user => {
+    users.push(user.name);
+  });
+  renderUserList();
+});
+
+const renderChat = () => {
   let html = "";
   messages.forEach((message) => {
     const row = template.replace("%MESSAGE", message);
-    html+=row;
+    html += row;
   });
   chat.innerHTML = html;
   window.scrollTo(0, document.body.scrollHeight);
-}
+};
+
+const renderUserList = () => {
+  userListElement.innerHTML = "";
+  users.forEach((user) => {
+    const row = `<li class="list-group-item">${user}</li>`;
+    userListElement.innerHTML += row;
+  });
+};
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const http = require("http");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-const { Server } = require('socket.io'); // importazione oggetto Server da socket.io
+
+const { Server } = require('socket.io');
 const conf = JSON.parse(fs.readFileSync("./conf.json"));
+const port = conf.port
 
 app.use(bodyParser.json());
 app.use(
@@ -18,18 +20,28 @@ app.use("/", express.static(path.join(__dirname, "public")));
 const server = http.createServer(app);
 const io = new Server(server);
 
+let userList = [];
 
 io.on('connection', (socket) => {
    console.log("socket connected: " + socket.id);
-   io.emit("chat", "new client: " + socket.id);
+
+   socket.on('setName', (name) => {
+      userList.push({ socketId: socket.id, name: name });
+      io.emit("list", userList);
+   });
+
    socket.on('message', (message) => {
-      const response = socket.id + ': ' + message;
+      const user = userList.find(user => user.socketId === socket.id);
+      const response = user ? user.name + ': ' + message : message;
       console.log(response);
       io.emit("chat", response);
    });
+
+   socket.on('disconnect', () => {
+      userList = userList.filter(user => user.socketId !== socket.id);
+      io.emit("list", userList);
+   });
 });
-
-server.listen(conf.port, () => {
-  console.log("server running on port: " + conf.port);
-
+server.listen(port, () => {
+   console.log(`Server is running on port ${port}`);
 });
